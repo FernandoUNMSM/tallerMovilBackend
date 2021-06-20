4d419996fd11c70dac74ffa23dda60174506087a
Your Name <you@example.com>
2021-06-20 02:02:11 -0500

    Querys Courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,50 +1,66 @@
 const express = require('express')
 const router = express.Router()
+const pool = require('../src/database');
 // const Course = require('./../models/Courses')
 // const User = require('./../models/User')
 
 const userExtractor = require('./../middleware/userExtractor')
 
 router.get('/courses', userExtractor, async (req, res, next) => {
+  
   // const {courses: coursesId} = await User.findById(req.userId)
   //Aqui va el query de buscar los cursos de un usuario
+  const {iduser} = req.body;
+  try{
+    let list
 
-  // console.log(coursesId)
-  // const courses = await Course.find({'_id': {$in: [...coursesId]}})
-  // Buscar varios documentos con un array de ids
-  res.status(200).json({
-    courses
-  })
+    list = await pool.query('SELECT * FROM heroku_b3e0382f6ba83ba.cursos WHERE usuario_id = ?', [iduser]);
+    res.status(200).json({
+      courses
+    })
+
+  }catch(err){
+    next(err);
+  }
+
+  
 })
 
 router.get('/courses/:id', userExtractor, async (req, res, next) => {
-  const {id: courseId} = req.params
-
+  
   // const course = await Course.findById(courseId)
   //Aqui va el query para obtener un curso especifico por su id
-
-  res.status(200).json({
-    course
-  })
+  const { id } = req.params;
+  try{
+    const course = await pool.query('SELECT * FROM heroku_b3e0382f6ba83ba.cursos WHERE curso_id = ?', [id]);
+    res.status(200).json({
+      data: course[0],
+    });
+  }catch(err){
+    next(err)
+  }
+  
 })
 
 router.post('/courses', userExtractor, async (req,  res, next) => {
-  const { name } = req.body
-
-  // const user = await User.findById(req.userId)
-
-
-  // const newCourse = new Course({
-  //   name,
-  //   user: user._id
-  // })
+  
+  //Aqui va el query para guardar un curso
 
   try {
-    // const savedCourse = await newCourse.save()
-    //Aqui va el query para guardar un curso
+    const { usuario_id ,categoria_id, curso_nombre, description, conoci_previo, privacidad } = req.body
 
-    // user.courses = user.courses.concat(savedCourse._id)
-    // await user.save()
+    const newCourse = {
+      usuario_id,
+      categoria_id,
+      curso_nombre,
+      description,
+      conoci_previo,
+      privacidad
+    }
+    
+    await pool.query('INSERT INTO heroku_b3e0382f6ba83ba.cursos SET ? ', newCourse);
+    const savedCourse = await pool.query('SELECT * FROM heroku_b3e0382f6ba83ba.cursos WHERE curso_nombre = ?', [curso_nombre]);
+    
 
     res.status(201).json(savedCourse)//Aca se debe de enviar el nuevo curso creado
   } catch (e) {
