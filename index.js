52010f17cfaf6c91948b8d061209fb241c5fb888
Oscar Fernando <oscar.cabellos@unmsm.edu.pe>
2021-06-20 21:36:30 -0500

    2

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
-
+app.use(cors())
+app.use(express.json())
 
 const users = require('./routes/users')
 const login = require('./routes/login')
@@ -15,14 +16,9 @@ const course = require('./routes/courses')
 const notFound = require('./middleware/notFound')
 const errors = require('./middleware/errors')
 
-
-
-app.use(cors())
-app.use(express.json())
 app.use(login)
 app.use(users)
 app.use(course)
-
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -39,8 +35,4 @@ const server = app.listen(PORT, () => {
   console.log(`La api esta en http://localhost:${PORT}`);
 })
 
-
-
 module.exports = { app, server }
-
-
