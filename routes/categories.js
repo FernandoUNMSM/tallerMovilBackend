08ce4be685e473744be6320704f09278f640b21c
Oscar Fernando <oscar.cabellos@unmsm.edu.pe>
2021-07-18 12:08:44 -0500

    comentarios categorias y cursos

--- routes/categories.js
+++ routes/categories.js
@@ -3,8 +3,13 @@ const router = express.Router()
 const pool = require('../src/database');
 
 router.get('/categories', async (req, res, next) => {
+  // Ruta para listar las categorias
+
   try{
+    // Aqui va el query para listar las categorias
     const categories = await pool.query('SELECT * FROM heroku_b3e0382f6ba83ba.categoria');
+    
+    //Respuesta a la peticion
     res.status(200).json({
       categories
     })
