d697c3982ff527b72e07a92ca22ab1a823853a1c
Rodrigo QP <rodrigoqp0722@gmail.com>
2021-08-29 18:32:58 -0500

    Para hacer un pull

--- sonar-project.js
+++ sonar-project.js
@@ -6,13 +6,13 @@ sonarqubeScanner(
       'sonar.sources': '.',
       'sonar.tests': 'tests',
       'sonar.host.url': 'http://localhost:9000',
-      'sonar.projectKey': 'aprendeenlineabackend',
+      'sonar.projectKey': 'Backend_3',
       'sonar.inclusions': '**', // Entry point of your code
       'sonar.test.inclusions': 'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx,tests/*.test.js',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
       'sonar.testExecutionReportPaths': 'coverage/test-reporter.xml',
       'sonar.login': 'admin',
-      'sonar.password': process.env.PASSWORDSONAR
+      'sonar.password': 'HolaMundo'
     }
   }, () => { 
     console.log('sonar is running')
